9d1f902e0a6130f120104ff144509bec746e3576
Guilherme Gomes <guigo@guigo.org>
2015-07-08 09:10:32 -0300

    Gulp e Package
    Inclusão do Package.json e ajuste nos caminhso do deploy do Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,23 @@ var gulp = require('gulp'),
     cdnizer = require("gulp-cdnizer");
 
 gulp.task('styles', function() {
-  return gulp.src('src/css/**/*.min.css')
+  return gulp.src('Content/onlinesites.*.css')
     .pipe(minifycss())
-    .pipe(gulp.dest('deploy/assets/css'))
+    .pipe(gulp.dest('deploy/Content'))
     .pipe(notify({ message: 'Task Styles Concluída' }));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')
+  return gulp.src('js/onlinesites*.js')
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('deploy/assets/js'))
+    .pipe(gulp.dest('deploy/js'))
     .pipe(notify({ message: 'Task Script Concluída' }));
 });
 
 gulp.task('images', function() {
-  return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('deploy/assets/images'))
+  return gulp.src('images/*')
+    .pipe(gulp.dest('deploy/images'))
     .pipe(notify({ message: 'Tak Images Concluída' }));
 });
 
@@ -38,48 +38,44 @@ gulp.task('bowertocdn', function(){
     return gulp.src(['*.html', '*.asp'])
     .pipe(cdnizer([
         {
-            file: 'assets/components/jquery/dist/jquery.min.js',
+            file: 'bower_components/jquery/dist/jquery.min.js',
             cdn: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
         },
         {
-            file: 'assets/components/bootstrap/dist/css/bootstrap.min.css',
+            file: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
             cdn: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'
         },
         {
-            file: 'assets/components/bootstrap/dist/js/bootstrap.min.js',
+            file: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
             cdn: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'
         },
         {
-            file: 'assets/components/font-awesome/css/font-awesome.min.css',
+            file: 'bower_components/fontawesome/css/font-awesome.min.css',
             cdn: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
-        },
+        },        
         {
-            file: 'assets/components/video.js/dist/video-js/video-js.min.css',
-            cdn: 'https://vjs.zencdn.net/4.12/video-js.css'
-        },
-        {
-            file: 'assets/components/video.js/dist/video-js/video.js',
-            cdn: 'https://vjs.zencdn.net/4.12/video.js'
-        },
-        {
-            file: 'assets/components/jQuery-Mask-Plugin/dist/jquery.mask.min.js',
-            cdn: 'http://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js'
-        },
-        {
-            file: 'assets/components/jquery-validation/dist/jquery.validate.min.js',
+            file: 'bower_components/jquery-validation/dist/jquery.validate.min.js',
             cdn: 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js'
         },
         {
-            file: 'assets/components/jquery-validation/dist/additional-methods.min.js',
+            file: 'bower_components/jquery-validation/dist/additional-methods.min.js',
             cdn: 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.min.js'
+        },
+        {
+            file: 'js/onlinesites.js',
+            cdn: 'js/onlinesites.min.js'
+        },
+        {
+            file: 'js/onlinesites.mobile.js',
+            cdn: 'js/onlinesites.mobile.min.js'
         }
     ]))
     .pipe(gulp.dest("deploy"))
-    .pipe(notify({ message: 'Tak Bower-to-CDN Concluída' }));
+    .pipe(notify({ message: 'Task Bower-to-CDN Concluída' }));
 });
 
 gulp.task('clean', function(cb) {
-    del(['deploy', 'deploy/assets/css', 'deploy/assets/js', 'deploy/assets/images'], cb)
+    del(['deploy/Content/css', 'deploy/js', 'deploy/images'], cb)
 });
 
 gulp.task('default', ['clean'], function() {
@@ -87,11 +83,11 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/css/**/*.min.css', ['styles']);
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch('src/images/**/*', ['images']);
-  gulp.watch(['*.html', '*.asp'], ['bowertocdn']);
+  gulp.watch('Content/onlinesites.*.css', ['styles']);
+  gulp.watch('js/onlinesites*.js', ['scripts']);
+  gulp.watch('images', ['images']);
+  gulp.watch(['*.html'], ['bowertocdn']);
   livereload.listen();
-  gulp.watch(['src/**']).on('change', livereload.changed);
+  gulp.watch(['**']).on('change', livereload.changed);
 
 });
